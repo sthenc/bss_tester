d5b0eef27329a87874ff44285e6e5d1d15e6935e
Stjepan Henc <sthenc@gmail.com>
2015-01-12 16:09:33 +0100

    prekid zbog predavanja

--- weighted_mixer.py
+++ weighted_mixer.py
@@ -11,32 +11,76 @@ from a_weighting import itu_r_468_amplitude_weight
 from detect_speech import detect_speech
 
 
-def weighted_mixer(y1, y2, sr, intervals, nmels, hopl):
-""" """
+def weighted_mixer(y1, y2, sr, nmels, hopl):
+	"""Mix speech with the noise at the specified SNR
+	
+	Args:
+		y1 - speech signal
+		y2 - noise/music
+		sr - samplerate
+		nmels - number of mel bins
+		hopl - hop lenght
+		
+	Returns:
+	
+		np.array containing 16-bit resolution sound samples 
+	
+	Note:
+		doesn't work with stereo yet
+	"""
+
+	intervals = detect_speech(y1, sr, hopl)
+
+	# compute mel spectrogram
+	S1 = lbr.feature.melspectrogram(y1, sr=sr, n_fft=2048, hop_length=hopl, n_mels=nmels)
+	S2 = lbr.feature.melspectrogram(y2, sr=sr, n_fft=2048, hop_length=hopl, n_mels=nmels)
+
+	# get frequencies for bins	
+	mel_freqs = lbr.mel_frequencies(n_mels=nmels, fmin=0, fmax=sr/2)
+
+	itu_r_468 = itu_r_468_amplitude_weight()
+
+	# compute a_weighting coefficient for every bin
+	log_aw = np.array(itu_r_468(mel_freqs))
+
+	# compute logarithm of amplitude and apply itu-r 468 a_weighting
+	log_S1 = lbr.logamplitude(S1, ref_power=np.max) + log_aw[:, np.newaxis]
+	log_S2 = lbr.logamplitude(S2, ref_power=np.max) + log_aw[:, np.newaxis]
+
+	gain = 0.5
+	
+	len_ret = min(len(y1), len(y2))
+	
+	y_ret = y1[0:len_ret] + gain * y2[0:len_ret] 
+	
+	return y_ret
+
+def show_spectrogram(y, sr, n_fft, nmels, hopl):
+
+	S = lbr.feature.melspectrogram(y, sr=sr, n_fft=2048, hop_length=hopl, n_mels=nmels)
+	
+	log_S = lbr.logamplitude(S, ref_power=np.max)
+	
+	
+	
+	time.sleep(3)
+
 if __name__ == "__main__":	
 	snr = -3
 
 	y1, sr = lbr.load('speech.wav', 16000)
 	y2, sr = lbr.load('noise.wav', 16000) # have to guess samplerate, otherwise the code might hang
-
-	intervals = detect_speech(y1, sr, hopl)
-
+	
 	nmels = 128
 	hopl = 64
-	# compute mel spectrogram
-	S1 = lbr.feature.melspectrogram(y1, sr=sr, n_fft=2048, hop_length=hopl, n_mels=nmels)
-	S2 = lbr.feature.melspectrogram(y2, sr=sr, n_fft=2048, hop_length=hopl, n_mels=nmels)
-
-	# get frequencies for bins
-	mel_freqs = lbr.mel_frequencies(n_mels=nmels, fmin=0, fmax=sr/2)
-
-	itu_r_468 = itu_r_468_amplitude_weight()
-
-	log_aw = np.array(itu_r_468(mel_freqs))
-
-	log_S1 = lbr.logamplitude(S1, ref_power=np.max) + log_aw[:, np.newaxis]
-	log_S2 = lbr.logamplitude(S2, ref_power=np.max) + log_aw[:, np.newaxis]
-
+	
+	y_out = weighted_mixer(y1, y2, sr, nmels, hopl)
+	
+	show_spectrogram(y_out, sr, 2048, nmels, hopl)
+	print (y_out)
+	lbr.output.write_wav('output.wav', y_out, sr, normalize=True) # hm...
+	
+	
 	##lbr.display.specshow(log_S1, sr=sr, hop_length=64, x_axis='time', y_axis='mel')
 	#lbr.display.specshow(log_S1, sr=sr, hop_length=64, x_axis='time', y_axis='mel')
 	##lbr.display.specshow(S1/np.max(S1), sr=sr, hop_length=64, x_axis='time', y_axis='mel')

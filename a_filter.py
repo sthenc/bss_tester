47088abcefdfc52e03e42e474de6a45fbb3b505f
Stjepan Henc <sthenc@gmail.com>
2015-01-18 21:14:40 +0100

    Resuming development 2 days before results presentation, and 3 weeks before final submission

---
+++ a_filter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# stjepan.henc@fer.hr
+
+import numpy as np # no problem
+import librosa as lbr 
+import matplotlib.pyplot as plt
+from scipy.signal import lfilter, freqz
+
+import time
+
+from a_weighting2 import itu_r_468_amplitude_weight
+
+def a_filter(y, intervals)
+	snr = 0
+
+	return snr

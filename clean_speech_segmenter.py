8194ad8279209482bf20f226a51de4a982ee3ef3
Stjepan Henc <sthenc@gmail.com>
2015-01-19 22:11:13 +0100

    Clean speech segmenter finished

--- clean_speech_segmenter.py
+++ clean_speech_segmenter.py
@@ -12,14 +12,83 @@ from subtitle import SrtSubtitle, Subtitle, SubtitleLine
 durmax = 90
 durmin = 70
 
-input_wav = '01.wav'
-input_srt = '01.srt'
+def connect_subs(titlovi):
+	ret = []
+	
+	tmpbeg = -1
+	tmpend = -1
+	tmptext = ""
+	
+	# unnecessary
+	#tmpfin = titlovi[-1].end	
+	
+	#durtot = tmpfin - tmpbeg
+	
+	for i in range(0, len(titlovi)):
+				
+		tmpend = titlovi[i].end
+		tmptext = tmptext + '\n' + titlovi[i].text.strip()
 
-s = SrtSubtitle()
+		if tmpbeg == -1:
+			tmpbeg = titlovi[i].start
+			tmptext = tmptext.strip()
+		
+		if tmpend - tmpbeg > durmin:
+			
+			ret.append([tmpbeg, tmpbeg + min(tmpend-tmpbeg, durmax), tmptext])
+			tmpbeg = -1
+			tmpend = -1
+			tmptext = ""
+		
+	# we will loose some data on the end, but we don't care, it's less than durmin
+	
+	return ret
 
-s.deserialize(input_srt)
+def cut_audio(y, sr, start, end):
+	start = max(start * sr, 0)
+	end = min(end * sr, len(y))
 
-titlovi = s.get_subs()
+	return y[start:end]
 
-print(s)
+input_folder = '/mnt/data/Fer/diplomski/test_data3/unsegmented/'
 
+input_names = [ '%02d' % i for i in range(1, 11)]
+#input_wavs = '01.wav'
+#input_srts = '01.srt'
+
+N_out = 1
+output_folder_wav = '/mnt/data/Fer/diplomski/test_data3/clean_speech/'
+output_folder_srt = '/mnt/data/Fer/diplomski/test_data3/transcripts/'
+
+for name in input_names:
+
+	input_srt = input_folder + name + '.srt'
+	input_wav = input_folder + name + '.wav'
+
+	s = SrtSubtitle()
+
+	s.deserialize(input_srt)
+
+	titlovi = s.get_subs()
+
+	transkripti = connect_subs(titlovi)
+
+	#print(transkripti)
+
+	y, sr = lbr.load(input_wav, 16000)
+
+	for i in range(len(transkripti)):
+		
+		y_out = cut_audio(y,sr, transkripti[i][0], transkripti[i][1])
+		
+		lbr.output.write_wav(output_folder_wav + '%03d.wav' % N_out, y_out, sr, normalize=True)
+		
+		f = open(output_folder_srt + '%03d.txt' % N_out, 'w')
+		
+		f.write(transkripti[i][2])
+		
+		f.close()
+		
+		N_out += 1
+		
+		print("Generated %d file" % N_out)

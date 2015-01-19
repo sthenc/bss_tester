146b8019719b986936ff16d6001be2f856855300
Stjepan Henc <sthenc@gmail.com>
2015-01-19 19:32:45 +0100

    Moving into preparing speech files extracted from audiobooks

---
+++ clean_speech_segmenter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import numpy as np # no problem
+import librosa as lbr 
+import matplotlib.pyplot as plt
+
+from detect_speech import detect_speech
+
+input_file
+
+# maximum and minimum durations
+durmax = 90
+durmin = 70
